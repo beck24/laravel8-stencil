f36511b2ce7769d2dcaeb9dc53344466d6c2c5af
Matt Beckett <beck24@gmail.com>
2020-09-26 02:49:16 -0700

    sanctum/fortify for auth routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// authenticated routes
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
+
+// public routes
+Route::post('/login', 'Laravel\Fortify\Http\Controllers\AuthenticatedSessionController@store');
