0dd739d6932835dcc70b933b70a20408fcd9657e
Matt Beckett <beck24@gmail.com>
2020-09-25 17:58:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Installation
+
+Copy .env.example to .env and change any locally needed variables
+
+`docker-compose up -d`
+
+# Laravel
+
+Laravel handles back end as a stateless API
+
+Copy the `.env.example` to `.env` and change any required values
+
+Run the following
+
+`composer install`
+`php artisan key:generate`
+`php artisan cache:clear`
+
+# Stencil
+
+Stenciljs is the front end, it compiles into the public dir of laravel
+
+For development with hot-reloading components
+
+Log into the container:
+
+    docker exec -it laravel8stencil_webserver bash
+
+    cd ../stencil
+
+If installing for the first time: `npm install`
+
+    cp .env.example .env
+    npm start
+
+Visit http://localhost:3333 (or alternate port if specified in .env)
+
+# Front End Config
+
+Stencil has a .env file to hold environment variables such as API urls and public keys
+
+Note: the .env file is used at build time, it is not read live.  If values are changed in the .env file the front end needs to be rebuilt for the changes to go into effect.
+
+.env variables can only be read in the `config.service.ts` file
+
+eg.
+
+    API_URL=http://mbeckett.desktop
+
+In the .env replace `process.env.API_URL` in the config.service.ts file
+
+    constructor() {
+        this.values = {
+            API_URL: process.env.API_URL
+        };
+    }
+
+To access these variables anywhere else in the codebase import the config service and use the get method
+
+    import { ConfigService } from '../../services/config.service';
+
+    const apiUrl = ConfigService.get('API_URL');
+
+
+# Production Build
+
+To build the front end for production, with optimized code and pre-rendering, there is a laravel artisan command that unlinks the public storage, triggers the build (which wipes out the public dir) then re-links the storage.
+
+`php artisan deploy:frontend`
---
+++ stencil/stencil.config.ts
@@ -0,0 +1,78 @@
+import { Config } from '@stencil/core';
+import dotenv from 'dotenv';
+import { sass } from '@stencil/sass';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
+// https://stenciljs.com/docs/config
+
+// dotEnvPlugin will replace instances of process.env.MY_VARIABLE
+// with the string from the .env file
+// this is limited to the config.service.ts file to ensure
+// limited scope, and usage of the config service in the codebase
+const envVars = dotenv.config();
+const dotEnvPlugin = () => {
+  return {
+    name: 'env',
+    transform: function (sourceText, id) {
+        let code = sourceText;
+
+        if (id.includes('config.service.ts')) {
+          const replaceAll = (str, find, replace) => {
+            // https://stackoverflow.com/a/1144788/9238321
+            return str.replace(new RegExp(find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), replace);
+          };
+  
+          Object.keys(envVars.parsed).forEach(function (key) {
+              code = replaceAll(code, "process.env." + key, "\"" + envVars.parsed[key] + "\"");
+          });
+        }
+
+        return new Promise(function (resolve) {
+            return resolve({
+                id: id,
+                code: code,
+            });
+        });
+    },
+  };
+};
+
+
+export const config: Config = {
+  globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/app.scss',
+  taskQueue: 'async',
+  devServer: {
+    port: parseInt(envVars.parsed.STENCIL_DEV_SERVER_PORT)
+  },
+  outputTargets: [
+    {
+      type: 'www',
+      dir: envVars.parsed.LARAVEL_DIR + '/public',
+      empty: true,
+      serviceWorker: null,
+      baseUrl: envVars.parsed.BASE_URL,
+      copy: [
+        {
+          src: envVars.parsed.LARAVEL_DIR + '/public-laravel',
+          dest: './'
+        }
+      ]
+    },
+    {
+      type: 'docs-readme',
+      footer: 'Built by Matt, using Stencil',
+      dir: './docs',
+      strict: true
+    }
+  ],
+  plugins: [
+    dotEnvPlugin(),
+    sass()
+  ],
+  rollupPlugins: {
+    after: [
+      nodePolyfills(),
+    ]
+  }
+};
